f501c4ada45caaf862b220b59c382050e9ed76de
Missy Allan <missyallan@Melissas-MacBook-Air.local>
2016-09-08 09:36:56 -0400

    functions readme cats

---
+++ functionsPlayground.playground/Contents.swift
@@ -0,0 +1,49 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func sayHello() {
+    let greeting = "Hello There!";
+    print (greeting)
+}
+
+sayHello()
+
+sayHello()
+
+func sayGoodbye() {
+    let farewell = "Goodbye, world."
+    print(farewell)
+}
+
+sayGoodbye()
+
+
+func sayHelloToGarfield() {
+print("Hello, Garfield, why do you sleep so much?")
+}
+
+func sayHelloToBuster() {
+print("Hello, Buster, why do you sleep so much?")
+}
+
+func sayHelloToScratchy() {
+print("Hello, Scratchy, why do you sleep so much?")
+}
+
+func sayHelloToCat(catName:String) {
+print ("Hello, \(catName), why do you sleeo so much?")
+}
+
+sayHelloToCat("Felix")
+
+sayHelloToCat("Mittens")
+
+sayHelloToCat("Lawrence")
+
+let catName = "Socks"
+sayHelloToCat(catName)
+
+
